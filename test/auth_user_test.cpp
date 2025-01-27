fa3eb5d3697f0796966d08e05737c45090bf1153
gulliver <gulliver@traumkristalle.net>
2025-01-27 14:20:55 +0100

    added test for validate user (name) reformatted source for tests

--- test/auth_user_test.cpp
+++ test/auth_user_test.cpp
@@ -32,38 +32,38 @@ using asio_error_code = asio::error_code;
 
 TEST_CASE("user create")
 {
-  User user;
-  REQUIRE(user.id().empty());
-  REQUIRE(user.hash().empty());
+	User user;
+	REQUIRE(user.id().empty());
+	REQUIRE(user.hash().empty());
 
-  REQUIRE(user.validate_password("")==false);
-  REQUIRE(user.validate_password("x")==false);
+	REQUIRE(user.validate_password("") == false);
+	REQUIRE(user.validate_password("x") == false);
 }
 
 TEST_CASE("user id")
 {
-  User user;
-  user.id("tester");
+	User user;
+	user.id("tester");
 
-  REQUIRE(user.id()=="tester");
+	REQUIRE(user.id() == "tester");
 }
 
 TEST_CASE("user password hash")
 {
-  User user;
+	User user;
 
-  const std::string password = "my_user_password";
-  user.password(password);
+	const std::string password = "my_user_password";
+	user.password(password);
 
-  // we check the hash against rehashed password
-  REQUIRE(BCrypt::validatePassword(password,user.hash())==true);
+	// we check the hash against rehashed password
+	REQUIRE(BCrypt::validatePassword(password, user.hash()) == true);
 
-  user.password("");
-  // fails on empty password
-  REQUIRE(BCrypt::validatePassword(password,user.hash())==false);
+	user.password("");
+	// fails on empty password
+	REQUIRE(BCrypt::validatePassword(password, user.hash()) == false);
 
-  user.password("pwd2");
-  REQUIRE(BCrypt::validatePassword("pwd2",user.hash())==true);
+	user.password("pwd2");
+	REQUIRE(BCrypt::validatePassword("pwd2", user.hash()) == true);
 }
 
 TEST_CASE("user email")
@@ -74,70 +74,77 @@ TEST_CASE("user email")
 
 TEST_CASE("user validate password")
 {
-  User user;
+	User user;
 
-  const std::string password = "my_user_password";
-  user.password(password);
+	const std::string password = "my_user_password";
+	user.password(password);
 
-  // we check the hash against rehashed password
-  REQUIRE(user.validate_password(password)==true);
-  REQUIRE(user.validate_password("wrong password")==false);
+	// we check the hash against rehashed password
+	REQUIRE(user.validate_password(password) == true);
+	REQUIRE(user.validate_password("wrong password") == false);
 
-  user.password("");
-  // fails on empty password
-  REQUIRE(user.validate_password(password)==false);
+	user.password("");
+	// fails on empty password
+	REQUIRE(user.validate_password(password) == false);
 
-  user.password("pwd2");
-  REQUIRE(user.validate_password("pwd2")==true);
-  REQUIRE(user.validate_password("not pwd2")==false);
+	user.password("pwd2");
+	REQUIRE(user.validate_password("pwd2") == true);
+	REQUIRE(user.validate_password("not pwd2") == false);
 }
 
 TEST_CASE("userdb create")
 {
-  AuthDb& user_db=AuthDb::get();
+	AuthDb& user_db = AuthDb::get();
 }
 
 TEST_CASE("userdb add_user")
 {
-  AuthDb& user_db=AuthDb::get();
-  User new_user;
-  new_user.id("tester").password("tester_password");
-  auto result = user_db.add_user(new_user);
-  REQUIRE(result.has_value());
-  auto get_new_user = user_db.getUser("tester");
+	AuthDb& user_db = AuthDb::get();
+	User new_user;
+	new_user.id("tester").password("tester_password");
+	auto result = user_db.add_user(new_user);
+	REQUIRE(result.has_value());
+	auto get_new_user = user_db.getUser("tester");
 
-  REQUIRE(get_new_user->id()=="tester");
-  REQUIRE(get_new_user->validate_password("tester_password"));
+	REQUIRE(get_new_user->id() == "tester");
+	REQUIRE(get_new_user->validate_password("tester_password"));
 }
 
 TEST_CASE("userdb get_user")
 {
-  AuthDb& user_db=AuthDb::get();
-  auto user = user_db.getUser("does not exist");
-  
-  REQUIRE(!user);
+	AuthDb& user_db = AuthDb::get();
+	auto user = user_db.getUser("does not exist");
+
+	REQUIRE(!user);
 }
 
 TEST_CASE("userdb del_user")
 {
-  AuthDb& user_db=AuthDb::get();
-  User new_user;
-  new_user.id("tester").password("tester_password");
-  user_db.add_user(new_user);
+	AuthDb& user_db = AuthDb::get();
+	User new_user;
+	new_user.id("tester").password("tester_password");
+	user_db.add_user(new_user);
 
-  // bas case: delete not existing
-  auto result = user_db.delete_user("not_existing");
-  REQUIRE(result == AuthDb::eAuthDbResult::NOT_FOUND);
+	// bas case: delete not existing
+	auto result = user_db.delete_user("not_existing");
+	REQUIRE(result == AuthDb::eAuthDbResult::NOT_FOUND);
 
-  // good case: delete previously added
-  result = user_db.delete_user("tester");
-  REQUIRE(result == AuthDb::eAuthDbResult::OK);
+	// good case: delete previously added
+	result = user_db.delete_user("tester");
+	REQUIRE(result == AuthDb::eAuthDbResult::OK);
 }
 
 TEST_CASE("userdb validate user(name)")
 {
-  // TODO create function and testcase
-CHECK(false);
+	AuthDb& user_db = AuthDb::get();
+	User new_user;
+	auto result = user_db.delete_user("tester");
 
+	new_user.id("tester").password("tester_password");
+	auto a1 =  user_db.add_user(new_user);
+	REQUIRE(a1.error() == AuthDb::eAuthDbResult::OK);
+
+	auto a2 = user_db.add_user(new_user);
+	REQUIRE(a1.error() == AuthDb::eAuthDbResult::NOT_VALID);
 }
 
